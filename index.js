52fca76754954c7874aa7e881765b87d77e809f5
exoad <45053009+exoad@users.noreply.github.com>
2021-02-22 13:51:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
-var keepalive = require('./keep_alive.js');
+var keepalive = require('./server.js');
 
 client.once(`ready`, () => {
     console.log (`${client.user.tag} is now online!`)
